515f7ab9f3547ade4262270a9bce3496f6c884f9
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-11-16 15:51:48 +0100

    Updated to use FX GUI.

--- src/tp2/StockClient.java
+++ src/tp2/StockClient.java
@@ -1,24 +1,27 @@
 package tp2;
 
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class StockClient {
-	
-	public static void main(String[] args) {
-
-		try {
-			System.setSecurityManager(new SecurityManager());
-			Registry registry = LocateRegistry.getRegistry(1099);
-			IStock stockImpl =  (IStock) registry.lookup("stockImpl");
-					
-			State state = stockImpl.state("cool");
-			System.out.println(state.getLastOp());
-			
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+public class StockClient extends Application {
 		
+	public static void main(String[] args) {
+		launch(args);
 	}
+	
+	
+	@Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Tp2Client.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle("Tp2 Client Side");
+        
+        stage.show();
+    }
 	
 }

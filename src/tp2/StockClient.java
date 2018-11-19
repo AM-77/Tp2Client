7e173342a11f46d6631ae4291c85a6fb8b34039c
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-11-19 17:20:44 +0100

    Tested

--- src/tp2/StockClient.java
+++ src/tp2/StockClient.java
@@ -1,35 +1,17 @@
 package tp2;
 
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-/*
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage; */
+import javafx.stage.Stage; 
 
-public class StockClient /* extends Application */{
-	
-	private static State state;
-	private static IStock stockImpl;
-	
+public class StockClient  extends Application {
+
 	public static void main(String[] args) throws Exception{
-		//launch(args);
-		
-		System.setProperty("java.rmi.server.hostname","192.168.43.239");
-		System.setSecurityManager(new SecurityManager());
-		Registry registry = LocateRegistry.getRegistry(1099);
-		stockImpl =  (IStock) registry.lookup("stockImpl");	
-		
-		System.out.println(stockImpl.sale("cool",  11000));
-		
-		state = stockImpl.state("cool");
-		
-		System.out.println(state.getLastOp());
-		
+		launch(args);
 	}
-/*	
+
 	@Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Tp2Client.fxml"));
@@ -40,5 +22,5 @@ public class StockClient /* extends Application */{
         
         stage.show();
     }
-	*/
+	
 }
